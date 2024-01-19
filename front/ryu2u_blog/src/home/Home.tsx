9114acded82ec5bc5519c6c91cf9d0ca13340383
chenhaitao <cht@njxdht.com>
2024-01-19 19:19:54 +0800

    1.19

---
+++ front/ryu2u_blog/src/home/Home.tsx
@@ -0,0 +1,28 @@
+
+import './home.css'
+
+export function Home() {
+    return (
+        <>
+            <nav className={"flex nav-head"}>
+                <div className={"home-icon"}>
+                    Ryu2u
+                </div>
+
+                <div className={"flex menu-list"}>
+                    <div> Ryu2u </div>
+                    <div> Ryu2u </div>
+                    <div> Ryu2u </div>
+                    <div> Ryu2u </div>
+                    <div> Ryu2u </div>
+                    <div> Ryu2u </div>
+                    <div> Ryu2u </div>
+                    <div> Ryu2u </div>
+                </div>
+
+
+            </nav>
+
+        </>
+    )
+}
